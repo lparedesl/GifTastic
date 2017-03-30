5feedec89a155a9369d581f40ce438c931e5c378
luis paredes <luis paredes>
2017-03-30 00:47:04 -0400

    updated api url

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -47,7 +47,7 @@ $(document).ready(function() {
 			lastIndex = buttonIndex;
 		}, 10);
 		var animal = $(this).attr("data-name");
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 		// var queryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + animal;
 
 		$.ajax({
